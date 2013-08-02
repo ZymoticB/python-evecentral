3982a6129e96f003dc6b4069d568533b9cf6bc9c
Tyler Dixon <tyler@chlystyk.com>
2013-08-02 16:43:41 -0700

    Add setup.py and README

---
+++ README.rst
@@ -0,0 +1,10 @@
+python-evecentral
+=============
+
+Super simple module that hits 2 external APIs. 
+
+It uses `Fuzzworks <[https://www.fuzzwork.co.uk/tools/api-typename-to-typeid/>`_ to get 
+the typeID from a typeName. 
+
+It then pulls prices from the `EVE-Central API <http://eve-central.com/home/develop.html>`_
+and caches them in memory
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "evecentral",
+    version = "0.0.1",
+    author = "Tyler Dixon",
+    author_email = "tyler@chlystyk.com",
+    description = "A small module to cache eve central prices in memory",
+    licence = "BSD",
+    keywords = "eve evecentral",
+    url = "https://github.com/ZymoticB/python-evecentral",
+    packages = ['evecentral'],
+    long_description=read('README.rst')
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Games/Entertainment",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
